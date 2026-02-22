65d2ba638c9f053d964be9e5529dce5443b5ebd0
marufmahadyarnob <137247420+marufmahadyarnob@users.noreply.github.com>
2026-02-22 21:27:25 +0600

    Add files via upload

---
+++ js/contact-me.js
@@ -0,0 +1,56 @@
+// typing animation
+
+new Typed(".typing",{
+
+strings:[
+
+"Assistant Teacher",
+
+"Savar Model Academy",
+
+"Web Designer",
+
+"Contact Me Anytime"
+
+],
+
+typeSpeed:60,
+
+backSpeed:40,
+
+loop:true
+
+});
+
+
+
+
+// particles
+
+particlesJS("particles-js", {
+
+particles: {
+
+number: { value: 80 },
+
+size: { value: 3 },
+
+color: { value: "#00ffff" },
+
+line_linked: {
+
+enable: true,
+
+color: "#00ffff"
+
+},
+
+move: {
+
+speed: 2
+
+}
+
+}
+
+});
